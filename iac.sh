53b93ea61447a7f3726fb9df02b9a43d2e83cc3f
marcocifuentes <marco.cifuentes@gmail.com>
2022-07-26 14:20:15 -0300

    atualizando...

--- iac.sh
+++ iac.sh
@@ -2,7 +2,7 @@
 
 ## -------------------------------------------------------------------- ##
 ## 	Script p/ criar diretorios, grupos, usuários e permissões       ##
-##	  Scritp - iac.sh, Versão 1.0, 25/07/2022		      	##
+##	  Scritp - iac.sh, Versão 1.1, 26/07/2022		      	##
 ##	  Certique-se do arquivo ter permissões de execução: chmod 744	##
 ## 	  p/ utilizar o Script, execute o comando: " sudo ./iac.sh " 	##
 ## -------------------------------------------------------------------- ##
@@ -25,25 +25,26 @@ groupadd GRP_VEN
 groupadd GRP_SEC
 
 ## Criando usuarios com senha "123" como default
-echo "Criando usuarios e definindo grupos pertencentes..."
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+echo "Criando usuarios e definindo grupos..."
+## NAO FUNCIONA COM "-crypt" - EX.: useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd carlos -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd debora -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd josefina -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
 
 ## Definindo permissoes nos diretorios
-echo "Definindo permissoes de grupos em diretorios..."
+echo "Definindo permissoes de grupos e diretorios..."
 chown root:GRP_ADM /public/adm
 chown root:GRP_VEN /public/ven
 chown root:GRP_SEC /public/sec
 chmod 770 /public/adm
 chmod 770 /public/ven
-chmod 770 /pubic/sec
+chmod 770 /public/sec
 chmod 777 /public/publico
 
 ## FIM
